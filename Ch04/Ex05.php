7319b168bce42768cc07ccd7807bfdb5100e328a
KitArun <kittinatarunthawi2547@gmail.com>
2024-12-02 15:31:59 +0700

    Ch2-4 Edit

---
+++ Ch04/Ex05.php
@@ -0,0 +1,15 @@
+<head>
+    <title>การใช้ Single Quote('') และ Double Quote("")</title>
+    </head>
+    <body>
+    <font size="4">
+    <?php
+        $name = "สมชาย";
+        $age = 30;
+        $country = "ประเทศไทย";
+        echo '<b>Single Quote:</b> My name is $name, $age years oldfrom $country<br />';
+        echo "<b>Double Quote:</b> My name is $name, $age years old from $country";
+    ?>
+    </font>
+    </body>
+    </html>
