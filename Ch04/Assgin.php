0ff135a6588d0aeca3c58035a70f131a4de5d083
KitArun <kittinatarunthawi2547@gmail.com>
2024-12-13 14:12:15 +0700

    Ch04_Assgin

---
+++ Ch04/Assgin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class Selector</title>
+    <link rel="stylesheet" type="text/css" href="Assgin.css">
+</head>
+<body>
+    <?php
+        echo '<h2 class="big">ประวัติ</h2>';
+        echo '<p class="normal">';
+        echo '<b>ชื่อ</b> นายกิตตินาถ <b>นามสกุล</b> อรุณทวีรุ่งโรจน์ <b>เลขประจำตัว</b> 6606021610052<br>';
+        echo '<b>Sec </b>- 1<br>';
+        echo '<b>สาขา</b> เทคโนโลยีสารสนเทศ <br>';
+        echo '<b>คณะ</b> เทคโนโลยีสารสนเทศและการจัดการอุตสาหกรรม';
+        echo '</p>';
+        echo '<p class="normal">By : ประวัติส่วนตัว</p>';
+        echo '<p class="small">Silver Wolf</p>';
+    ?>
+</body>
+</html>
