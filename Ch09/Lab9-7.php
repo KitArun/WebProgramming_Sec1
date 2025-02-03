2951e6652d393630c5b4f5b1f55c06f684e56dea
KitArun <kittinatarunthawi2547@gmail.com>
2025-02-03 15:16:11 +0700

    Lab9-7-total

--- Ch09/Lab9-7.php
+++ Ch09/Lab9-7.php
@@ -8,6 +8,7 @@
     <?php
     $maxRow = 10;
     $maxCol = 4;
+    $score = array();
     for ($r = 0; $r < $maxRow; $r++) {
         for ($c = 0; $c < $maxCol; $c++) {
             if ($c == 0) {
@@ -25,12 +26,16 @@
     echo "<tr><td width='80' align='center'>Homework</td>";
     echo "<td width='80' align='center'>Assignment</td>";
     echo "<td width='80' align='center'>Midterm</td>";
-    echo "<td width='80' align='center'>Final</td></tr>";
+    echo "<td width='80' align='center'>Final</td>";
+    echo "<td width='80' align='center'>100</td></tr>";
     for ($r = 0; $r < $maxRow; $r++) {
         echo "<tr>";
+        $rowTotal = 0;
         for ($c = 0; $c < $maxCol; $c++) {
             echo "<td align='center'>" . $score[$r][$c] . "</td>";
+            $rowTotal += $score[$r][$c];
         }
+        echo "<td align='center'>" . $rowTotal . "</td>";
         echo "</tr>";
     }
     echo "</table>";
