2b456cc9e7419e2c20b39d2886d48c6e7a7f65b0
KITTINAT <143639731+KitArun@users.noreply.github.com>
2025-03-18 19:03:13 +0700

    Update bookstore

--- bookStore/checkUser.php
+++ bookStore/checkUser.php
@@ -1,22 +1,33 @@
 <?php
 session_start();
+
 $Username = $_POST['Username'];
 $Password = $_POST['Password'];
 $hostname = "localhost";
 $username = "root";
 $password = "";
-$dbname = "session";
-$conn = mysqli_connect($hostname, $username, $password);
-if (!$conn)
+$dbname = "bookstore";
+
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+
+if (!$conn) {
     die("ไม่สามารถติดต่อกับ MySQL ได้");
-    mysqli_select_db($conn, $dbname) or die("ไม่สามารถเลือกฐานข้อมูล session ได้");
-    $sqltxt = "SELECT * FROM login where username = '$Username'";
-    $result = mysqli_query($conn, $sqltxt);
-    $rs = mysqli_fetch_array($result);
+}
+
+$sqltxt = "SELECT * FROM login WHERE username = ?";
+$stmt = mysqli_prepare($conn, $sqltxt);
+
+mysqli_stmt_bind_param($stmt, "s", $Username);
+mysqli_stmt_execute($stmt);
+
+$result = mysqli_stmt_get_result($stmt);
+$rs = mysqli_fetch_array($result);
+
 if ($rs) {
     if ($rs['password'] == $Password) {
         $_SESSION['Username'] = $Username;
-        header("Location: welcome.php?Username=$Username");
+        header("Location: bookList1.php?Username=$Username");
+        exit();
     } else {
         echo "<br>Password not match.";
         echo "<br><a href='login.php'>คลิก กลับไปเพื่อ login</a>";
@@ -25,4 +36,7 @@ if ($rs) {
     echo "Not found Username " . $Username;
     echo "<br><a href='login.php'>คลิก กลับไปเพื่อ login </a>";
 }
+
+// Close the database connection
+mysqli_close($conn);
 ?>
